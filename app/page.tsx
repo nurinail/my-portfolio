10f89ce5cad176a46944e183ab121590428d7193
nail.nuri <nail.nuri@iktex.az>
2026-04-17 16:23:41 +0400

    feat: update package dependencies and improve linting scripts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/react";
 
 export default function Home() {
-	return <Box w="full" h="full" bg="brandPurple.100"></Box>;
+	return <Box h="100dvh" bg="brandPurple.100"></Box>;
 }
