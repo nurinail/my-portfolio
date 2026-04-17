f93efb05bbee5f3047bb7aefff73cd328cb710de
nail.nuri <nail.nuri@iktex.az>
2026-04-17 16:17:49 +0400

    feat: add virtual colors for semantic tokens in Chakra UI
    - Introduced base colors, border colors, background colors, foreground colors, text colors, alpha colors, button colors, icon colors, avatar colors, navigation colors, breadcrumb colors, tooltip colors, slider colors, toggle colors, and utility colors.
    - Combined all color definitions into a single  export for easier usage in the Chakra UI theme.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import { Box } from "@chakra-ui/react";
 
-
 export default function Home() {
-  return (
-  <Box w='full' h='full' bg='#1A0B2E'>
-
-  </Box>
-  );
+	return <Box w="full" h="full" bg="brandPurple.100"></Box>;
 }
